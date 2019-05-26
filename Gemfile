3c2df6960b14d56fc88b3b36b2b1635e6d93212c
Adam Zima <adam.zima@u2i.com>
2019-05-26 16:04:11 +0200

    Add minimal integration test case for total page views.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 ruby '2.6.3'
 
 gem 'rspec'
+gem 'simplecov', require: false, group: :test
--- spec/logs_parser_spec.rb
+++ spec/logs_parser_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../app/logs_parser'
+require 'tempfile'
 
 describe LogsParser do
   subject { described_class.new(log_file_name).run }
@@ -6,5 +7,35 @@ describe LogsParser do
 
   it 'runs' do
     subject
+  end
+
+  describe '#run' do
+    let(:logs) do
+      <<-LOGS
+/help_page/1 126.318.035.038
+/about/2 444.701.448.104
+/help_page/1 929.398.951.889
+      LOGS
+    end
+
+    let(:expected_output) do
+      <<-OUTPUT
+/help_page/1 2 visits
+/about/2 1 visits
+      OUTPUT
+    end
+
+    let(:test_log_file) do
+      file = Tempfile.new
+      file << logs
+      file.close
+      file
+    end
+    let(:log_file_name) {test_log_file.path}
+    after {test_log_file.unlink}
+
+    it 'lists webpages with most page views ordered from most pages views to less page views' do
+      expect { subject }.to output(expected_output).to_stdout
+    end
   end
 end
