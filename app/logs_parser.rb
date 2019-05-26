a386228000934eda38f09f0acb389be4d12f82e4
Adam Zima <adam.zima@u2i.com>
2019-05-26 16:35:28 +0200

    Basic implementation for LogsParser - Shameless Green!

--- app/logs_parser.rb
+++ app/logs_parser.rb
@@ -1,13 +1,21 @@
 class LogsParser
   def initialize(file_name)
     @file_name = file_name
+    @page_views = Hash.new { |hash, key| hash[key] = 0 }
   end
 
   def run
-    puts "Running logs parser for #{file_name}"
+    File.open(file_name).readlines.each do |line|
+      page, _ip = line.split(' ')
+      page_views[page] += 1
+    end
+
+    Array(page_views).sort_by { |_page, views| -views }.each do |page, views|
+      puts "#{page} #{views} visits"
+    end
   end
 
   private
 
-  attr_reader :file_name
+  attr_reader :file_name, :page_views
 end
